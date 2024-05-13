b472ad06fa771a76408e65e21a38380edbefcc05
Amanda Kaori <125595801+projamandakaori@users.noreply.github.com>
2024-05-13 18:12:48 -0300

    Dao

--- bd.sql
+++ bd.sql
@@ -2,7 +2,7 @@ create database bob;
 use bob;
 
 create table usuario(
-    id int auto_increment not nul,
+    id int auto_increment not null,
     login varchar(120) null,
     senha varchar(120) null,
     email varchar(200) null,
